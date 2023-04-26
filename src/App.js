d0db56f82574910d26bb850a0107b57d2729a7f0
V1P3RS <86308839+V1P3RS@users.noreply.github.com>
2023-04-26 14:05:10 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
+const Dummy_Expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.1,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "Sonpapdi",
+    amount: 33.1,
+    date: new Date(2022, 6, 18),
+  },
+  {
+    id: "e3",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e4",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2023, 2, 28),
+  },
+  {
+    id: "e5",
+    title: "Miami",
+    amount: 946.67,
+    date: new Date(2023, 6, 28),
+  },
+  {
+    id: "e6",
+    title: "wifi",
+    amount: 24.67,
+    date: new Date(2023, 9, 28),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(Dummy_Expenses);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Expenses.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import ExpenseFilter from "./ExpenseFilter";
+import ExpenseChart from "./ExpensesChart";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No Expenses Found!</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        key={expense.id}
+      />
+    ));
+  }
+
+  return (
+    <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      <ExpenseChart expenses={filteredExpenses} />
+      {expensesContent};
+    </Card>
+  );
+};
+
+export default Expenses;
